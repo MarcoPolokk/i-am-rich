5eff426d79677bd51314e3cfbf58c266df80eb6d
MarcoPolokk <58775058+MarcoPolokk@users.noreply.github.com>
2021-01-12 23:20:12 +0100

    Initial Commit

---
+++ swift-ui/swift_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_uiApp.swift
+//  swift-ui
+//
+//  Created by Paweł Kozioł on 12/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct swift_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
