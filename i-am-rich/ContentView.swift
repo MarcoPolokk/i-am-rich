d015cdb6d17c64418c471a3aa9290d6cdb50e58e
MarcoPolokk <58775058+MarcoPolokk@users.noreply.github.com>
2021-01-16 22:17:48 +0100

    Project completed with SwiftUI

--- i-am-rich/ContentView.swift
+++ i-am-rich/ContentView.swift
@@ -9,8 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(.systemTeal)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("I am Rich!")
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                    .padding()
+                    .font(.system(size: 42))
+                Image("diamond").resizable().aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200, alignment: .center)
+            }
+        }
     }
 }
 
